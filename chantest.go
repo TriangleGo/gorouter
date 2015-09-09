b32d771554a8e7e862cb6dd09983e87af3fd3af8
sorawa <thepowerover@163.com>
2015-09-09 17:32:03 +0800

    new test

--- chantest.go
+++ chantest.go
@@ -4,15 +4,23 @@ package main
 
 import (
 	_"strings"
-	"time"
+	_"time"
 	_"os"
 	"fmt"
 	_"runtime"
 	_"path/filepath"
-	"./lib/goconfig"
-	"gorouter/logger"
+	_"gorouter/logger"
+	"github.com/garyburd/redigo/redis"
+	"gorouter/lib/goconfig"
+	"encoding/json"
 )
 
+
+type Test struct  {
+	Name string 
+	Age int 
+	Userid int
+}
 
 func main () {
 	fmt.Printf("logger test \n")
@@ -24,19 +32,27 @@ func main () {
 	logLv,_ := c.GetInt64("log", "file_log_level");	
 	fmt.Printf("config file logLv %v \n",logLv)
 
-	log := logger.NewLogger()
-	log.SetOutputDir(`c:\`)
-	log.SetLogLevel(5,5)
-	log.Init("log")
-	for i:=0;i<99999;i++ {
-		log.Log(5,"i am the logger %v %d\n","hahahaha",i)	
-		time.Sleep(time.Millisecond * 1000)
-	}
-	log.Close()
+	pool := redis.NewPool(func()(redis.Conn, error){
+			return redis.Dial("tcp", "127.0.0.1:6379")},10)
+	pool.MaxActive = 10
+	
+	t := Test{Name:"John",Age:16,Userid:10086}
+	tJson, err := json.Marshal(t)
+
+	
+	conn := pool.Get()
+	i,err := conn.Do(`hset`,"domain","key",tJson)
+	fmt.Printf("i %v error %v \n",i,err)
+	
+	i,err = conn.Do(`hget`,"domain","key")
 	
 	
-	fmt.Printf("asdasd %v \n","123")
 	
+	fmt.Printf("ret string %s \n",i)
+	nt := Test{}
+	err = json.Unmarshal(i.([]byte),&nt)
+	fmt.Printf("get %v %v\n", nt,err)
+	conn.Close()
 }
 
 
